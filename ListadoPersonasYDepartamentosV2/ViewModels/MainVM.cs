bfd04af4b0b9c7ac032e549ffa933dba749c8a3c
aleleotta <114069815+aleleotta@users.noreply.github.com>
2024-04-08 21:11:24 +0200

    .

--- ListadoPersonasYDepartamentosV2/ViewModels/MainVM.cs
+++ ListadoPersonasYDepartamentosV2/ViewModels/MainVM.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace ListadoPersonasYDepartamentosV2.ViewModels
 {
-    internal class MainVM
+    internal class MainVM : INotifyPropertyChanged
     {
+        #region Attributes
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Constructors
+        public MainVM() { }
+        #endregion
+
+        #region Methods
+        #endregion
+
+        #region ViewModel
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        #endregion
     }
 }
