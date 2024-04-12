6d88da2a95c9e4145a51d0ebcebaaad69d49f760
aleleotta <114069815+aleleotta@users.noreply.github.com>
2024-04-12 14:33:24 +0200

    .

--- ListadoPersonasYDepartamentosV2/Views/MainPage.xaml.cs
+++ ListadoPersonasYDepartamentosV2/Views/MainPage.xaml.cs
@@ -1,10 +1,13 @@
-﻿namespace ListadoPersonasYDepartamentosV2.Views
+﻿using ListadoPersonasYDepartamentosV2.ViewModels;
+
+namespace ListadoPersonasYDepartamentosV2.Views
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = new MainVM();
         }
     }
 }
